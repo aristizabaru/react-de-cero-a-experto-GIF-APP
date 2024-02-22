1e7184fccab1a540e3d8021f8ffabf0897177961
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-22 06:51:42 -0500

    feat: utility function to fetch gif's data

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=qnKIqOSxp7pCK60QH2N1yRobgdn4GOBS&q=${category}&limit=5`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => (
+        {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    ));
+
+    console.log(gifs);
+
+    return gifs;
+};
