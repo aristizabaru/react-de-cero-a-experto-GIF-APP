8789a4beedbfbc38a4eeb7ce54e86b2ff37a4ef7
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-23 07:26:55 -0500

    feat (useFetchGif.js): implements custom hook
    Handle image loading with loading state

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const getImages = async () => {
+            const newImages = await getGifs(category);
+            setImages(newImages);
+            setIsLoading(false);
+        };
+
+        getImages();
+
+    }, [category]);
+
+    return {
+        images,
+        isLoading
+    };
+};
