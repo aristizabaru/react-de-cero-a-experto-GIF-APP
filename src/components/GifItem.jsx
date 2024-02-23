81d15a401d10ed804cb64d33c5cc0f471100bee4
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-23 07:06:23 -0500

    feat: add GitItem functional component
    Handles the card rendering, url and title.

---
+++ src/components/GifItem.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export const GifItem = ({ title, url }) => {
+    return (
+        <div className="card">
+            <img src={url} alt={title} />
+            <p>{title}</p>
+        </div>
+    );
+};
+
+GifItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+};
